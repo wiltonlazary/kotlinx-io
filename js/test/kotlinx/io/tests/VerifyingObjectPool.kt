eef517197a7b91bbe79e921f68de5e7b46ff8ce2
Roman Artemev <roman.artemev@jetbrains.com>
2019-11-29 19:59:07 +0300

    Make it work with JS IR

--- js/test/kotlinx/io/tests/VerifyingObjectPool.kt
+++ js/test/kotlinx/io/tests/VerifyingObjectPool.kt
@@ -3,7 +3,8 @@ package kotlinx.io.tests
 import kotlinx.io.pool.*
 
 @Suppress("NOTHING_TO_INLINE")
-internal inline actual fun identityHashCode(instance: Any): Int = js("Kotlin").identityHashCode(instance) as Int
+internal inline actual fun identityHashCode(instance: Any): Int = kotlin.js.identityHashCode(instance) as Int
+//internal inline actual fun identityHashCode(instance: Any): Int = js("Kotlin").identityHashCode(instance) as Int
 
 actual class VerifyingObjectPool<T : Any> actual constructor(delegate: ObjectPool<T>) : VerifyingPoolBase<T>(delegate) {
     override val allocated = HashSet<IdentityWrapper<T>>()
